2e215fccd5ac4ebe247f0d4a54d4719541aa331c
Anatoliy <ansid10@yandex.ru>
2020-09-10 13:09:48 +0300

    Updates

---
+++ precache-manifest.feddb21c6e55c64e3e78cd5319d19bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8318a70eadb54e59c067b2df731cb5db",
+    "url": "/personalArea-TS/index.html"
+  },
+  {
+    "revision": "56f0bef8757fcb4aea38",
+    "url": "/personalArea-TS/static/css/main.7333111f.chunk.css"
+  },
+  {
+    "revision": "12acef22422c1cc31148",
+    "url": "/personalArea-TS/static/js/2.46396fdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personalArea-TS/static/js/2.46396fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f0bef8757fcb4aea38",
+    "url": "/personalArea-TS/static/js/main.7345c47a.chunk.js"
+  },
+  {
+    "revision": "2d8807ae623ba75d4f83",
+    "url": "/personalArea-TS/static/js/runtime-main.388f251c.js"
+  }
+]);
