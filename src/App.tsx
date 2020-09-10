e9963b896d6dc91e1ed816d31e49e722dc76d25c
Anatoliy <ansid10@yandex.ru>
2020-09-10 13:02:35 +0300

    Repository "personalArea-TS" init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './App.css';
+
+export const App = (): JSX.Element => {
+  return (
+    <div className="app__wrapper">
+      <div className="app__content">
+        <h1>Личный кабинет</h1>
+      </div>
+    </div>
+  );
+};
